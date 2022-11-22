e4b2626c59d57866a6163d302ab8e6576caf28fa
DidierDefrance <didier@d-defrance.fr>
2022-11-22 14:30:31 +0100

    s

---
+++ AppliSpringSansSpringBoot/src/script/h2/set_env.bat
@@ -0,0 +1,11 @@
+
+REM set MVN_REPOSITORY=C:\ext\mvn-repository
+set MVN_REPOSITORY=C:\Users\d2fde\.m2\repository
+REM set MVN_REPOSITORY=C:\Users\formation\.m2\repository
+
+set MY_H2_DB_URL=jdbc:h2:~/mydbbank
+
+set PATH="C:\Program Files\eclipse\plugins\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_17.0.2.v20220201-1208\jre\bin"
+
+set H2_VERSION=2.1.214
+set H2_CLASSPATH=%MVN_REPOSITORY%\com\h2database\h2\%H2_VERSION%\h2-%H2_VERSION%.jar
--- AppliSpringSansSpringBoot/src/test/java/tp/appliSpring/dao/TestCompteDao.java
+++ AppliSpringSansSpringBoot/src/test/java/tp/appliSpring/dao/TestCompteDao.java
@@ -51,13 +51,14 @@ public class TestCompteDao {
 		Assertions.assertEquals("compte_a",compteRelu.getLabel());
 		Assertions.assertEquals(150.0,compteRelu.getSolde());
 		logger.debug("compteRelu apres miseAjour=" + compteRelu);
-		
+		/*
 		//+supprimer :
 		this.daoCompte.deleteById(compteSauvegarde.getNumero());
 		
 		//verifier bien supprimé (en tentant une relecture qui renvoi null)
 		Compte compteReluApresSuppression = this.daoCompte.findById(compteSauvegarde.getNumero()); 
 		Assertions.assertTrue(compteReluApresSuppression == null);
+		*/
 	}
 
 }
