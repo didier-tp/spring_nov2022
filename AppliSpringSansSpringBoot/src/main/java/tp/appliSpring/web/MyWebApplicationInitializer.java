01a94bceed9a691092cbfc8c3e9cea0c5152124a
DidierDefrance <didier@d-defrance.fr>
2022-11-23 11:54:49 +0100

    debut spring-mvc

--- AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/web/MyWebApplicationInitializer.java
+++ AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/web/MyWebApplicationInitializer.java
@@ -9,17 +9,50 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 
 import tp.appliSpring.core.MySpringApplication;
 
-public class MyWebApplicationInitializer implements WebApplicationInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		//java -Dspring.profiles.active=dev,initDataSet possible dans .bat ou .sh 
-		//qui démarre tomcat
-		System.setProperty("spring.profiles.active", "initDataSet");
-		AnnotationConfigWebApplicationContext contextSpringWeb = 
-				new AnnotationConfigWebApplicationContext();
-		contextSpringWeb.register(MySpringApplication.class);
-		servletContext.addListener(new ContextLoaderListener (contextSpringWeb ));
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	MyWebApplicationInitializer() {
+		System.out.println("MyWebApplicationInitializer ...");
 	}
 
+	protected String[] getServletMappings() {
+		return new String[] { "/mvc/*" }; // URL en :8080/.../mvc/...
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { MyWebAppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[0];
+	}
+
+	@Override
+	public void onStartup(ServletContext context) throws ServletException {
+		super.onStartup(context);
+		// String activeProfile = "";
+		String activeProfile = "initDataSet";
+		context.setInitParameter("spring.profiles.active", activeProfile);
+	}
 }
+
+/*
+ * public class MyWebApplicationInitializer implements WebApplicationInitializer
+ * {
+ * 
+ * @Override public void onStartup(ServletContext servletContext) throws
+ * ServletException { //java -Dspring.profiles.active=dev,initDataSet possible
+ * dans .bat ou .sh //qui démarre tomcat
+ * System.setProperty("spring.profiles.active", "initDataSet");
+ * AnnotationConfigWebApplicationContext contextSpringWeb = new
+ * AnnotationConfigWebApplicationContext();
+ * contextSpringWeb.register(MySpringApplication.class);
+ * servletContext.addListener(new ContextLoaderListener (contextSpringWeb )); }
+ * 
+ * }
+ */
