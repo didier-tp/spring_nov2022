ab37a96533cd6dea86f5169ebb7bff8c9e99c58d
DidierDefrance <didier@d-defrance.fr>
2022-11-23 16:42:58 +0100

    conversion dto

--- AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/web/rest/CompteRestCtrl.java
+++ AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/web/rest/CompteRestCtrl.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import tp.appliSpring.converter.GenericConverter;
 import tp.appliSpring.core.entity.Compte;
 import tp.appliSpring.core.service.ServiceCompte;
+import tp.appliSpring.dto.CompteDto;
 import tp.appliSpring.dto.Message;
 @RestController
 @RequestMapping(value="/api-bank/compte" , headers="Accept=application/json")
@@ -26,8 +28,8 @@ public class CompteRestCtrl {
 	//@RequestMapping(value="/{numCompte}" , method=RequestMethod.GET) 
 	@GetMapping(value="/{numCompte}")
 	/*
-	public Compte getCompteByNum(@PathVariable("numCompte") Long numCompte) { 
-	      return serviceCompte.rechercherCompteParNumero(numCompte) ;
+	public CompteDto getCompteByNum(@PathVariable("numCompte") Long numCompte) { 
+	      return GenericConverter.map(serviceCompte.rechercherCompteParNumero(numCompte),CompteDto.class) ;
 	}*/
 	public ResponseEntity<?> getCompteByNum(@PathVariable("numCompte") Long numCompte) { 
 	      Compte compte =serviceCompte.rechercherCompteParNumero(numCompte) ;
@@ -35,19 +37,20 @@ public class CompteRestCtrl {
 	    	  return new ResponseEntity<Message>( new Message("compte pas trouvé pour numero= " + numCompte),
 	    			                              HttpStatus.NOT_FOUND); //404
 	      else 
-	    	  return new ResponseEntity<Compte>(compte, HttpStatus.OK);//200
+	    	  return new ResponseEntity<CompteDto>( GenericConverter.map(compte,CompteDto.class), HttpStatus.OK);//200
 	}
 
 	
 	//URL http://localhost:8080/applispringSansSpringBoot/mvc/api-rest/compte
 	//ou  http://localhost:8080/applispringSansSpringBoot/mvc/api-rest/compte?soldeMini=120
 	@GetMapping(value="")
-	public List<Compte> getComptesByCriteria(@RequestParam(value="soldeMini",required=false)Double soldeMini) {
+	public List<CompteDto> getComptesByCriteria(@RequestParam(value="soldeMini",required=false)Double soldeMini) {
 		List<Compte> comptes = serviceCompte.rechercherTousComptes();
+		List<CompteDto> comptesDto = GenericConverter.map(serviceCompte.rechercherTousComptes(),CompteDto.class);
 	 if(soldeMini==null)
-	     return comptes;
+	     return comptesDto;
 	 else{
-	     return comptes.stream().filter( c -> (c.getSolde() >= soldeMini) ).toList();
+	     return comptesDto.stream().filter( c -> (c.getSolde() >= soldeMini) ).toList();
 	 }
 	}
 }
