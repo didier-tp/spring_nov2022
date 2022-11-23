1d5997ab944b48f7ba66734fc6162743ee697033
DidierDefrance <didier@d-defrance.fr>
2022-11-23 16:33:36 +0100

    s

--- AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/dto/CompteDto.java
+++ AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/dto/CompteDto.java
@@ -8,4 +8,42 @@ public class CompteDto {
 	private String label;
 	
 	private Double solde;
+	
+
+	public CompteDto() {
+		super();
+	}
+
+	public CompteDto(Long numero, String label, Double solde) {
+		super();
+		this.numero = numero;
+		this.label = label;
+		this.solde = solde;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(Double solde) {
+		this.solde = solde;
+	}
+	
+	
 }
