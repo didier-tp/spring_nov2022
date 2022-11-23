21f569e7d1c533ce15c60110f757bcec79fbd263
DidierDefrance <didier@d-defrance.fr>
2022-11-23 16:11:27 +0100

    ResponseEntity

---
+++ AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/dto/Message.java
@@ -0,0 +1,26 @@
+package tp.appliSpring.dto;
+
+public class Message {
+	private String message;
+	//...
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Message() {
+		super();
+	}
+
+	public Message(String message) {
+		super();
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
--- AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/web/rest/CompteRestCtrl.java
+++ AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/web/rest/CompteRestCtrl.java
@@ -3,6 +3,8 @@ package tp.appliSpring.web.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tp.appliSpring.core.entity.Compte;
 import tp.appliSpring.core.service.ServiceCompte;
+import tp.appliSpring.dto.Message;
 @RestController
 @RequestMapping(value="/api-bank/compte" , headers="Accept=application/json")
 public class CompteRestCtrl {
@@ -22,8 +25,17 @@ public class CompteRestCtrl {
 	//URL http://localhost:8080/applispringSansSpringBoot/mvc/api-rest/compte/1
 	//@RequestMapping(value="/{numCompte}" , method=RequestMethod.GET) 
 	@GetMapping(value="/{numCompte}")
+	/*
 	public Compte getCompteByNum(@PathVariable("numCompte") Long numCompte) { 
 	      return serviceCompte.rechercherCompteParNumero(numCompte) ;
+	}*/
+	public ResponseEntity<?> getCompteByNum(@PathVariable("numCompte") Long numCompte) { 
+	      Compte compte =serviceCompte.rechercherCompteParNumero(numCompte) ;
+	      if(compte == null)
+	    	  return new ResponseEntity<Message>( new Message("compte pas trouvé pour numero= " + numCompte),
+	    			                              HttpStatus.NOT_FOUND); //404
+	      else 
+	    	  return new ResponseEntity<Compte>(compte, HttpStatus.OK);//200
 	}
 
 	
