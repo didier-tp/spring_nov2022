b397068562cc5c65121f2f3f2e089c0d980cf0f2
DidierDefrance <didier@d-defrance.fr>
2022-11-23 16:24:50 +0100

    s

---
+++ AppliSpringSansSpringBoot/src/main/java/tp/appliSpring/dto/CompteDto.java
@@ -0,0 +1,11 @@
+package tp.appliSpring.dto;
+
+public class CompteDto {
+	private Long numero;
+	//private String numero;
+	//private Long num;
+	
+	private String label;
+	
+	private Double solde;
+}
